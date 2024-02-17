6d44f1d4aee6a8023797296d48c23561f9b104da
Karina Peres <karinaperesg@gmail.com>
2024-02-17 11:34:58 -0300

    Exercicio tabuada laço for

---
+++ Modulo_1/exercicios_sem1/tabuada_laco_for.js
@@ -0,0 +1,6 @@
+// Tabuada 5 laço for
+
+for (i = 0; i <= 10; i++) { // inicia i com valor 0, enquanto i for menor ou igual a 10, incremento i + i
+    const tabuada = 5 * i; // cria variavel "tabuada" igual a 5 multiplicado pelo i
+    console.log(`5 x ${i} = ${tabuada}`); // exibe o calculo, é usado a crase
+}
