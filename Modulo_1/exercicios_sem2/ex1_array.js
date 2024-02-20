60750a6f990067d0a83625a50272a6c3f6eeb093
Karina Peres <karinaperesg@gmail.com>
2024-02-20 16:59:52 -0300

    Inclusão Exercicio 1 Array, Semana 2

---
+++ Modulo_1/exercicios_sem2/ex1_array.js
@@ -0,0 +1,21 @@
+// Crie um array chamado frutas contendo três frutas diferentes. Em seguida, imprima no console a segunda fruta do array.
+// Desafio: utilize a biblioteca prompt-sync para receber cada uma das 3 frutas
+
+const prompt = require("prompt-sync")();
+const frutas = [];
+
+for (i = 0; i < 3; i++) {
+    let fruta = prompt(`Digite a ${i + 1}ª fruta: `);
+    if (frutas.includes(fruta)){ // includes verifica se a fruta digitada já está na lista
+        console.log (`A fruta ${fruta} já foi digitada.`);
+        i--; // volta a iteração para nova inserção        
+    } 
+    else {
+        frutas.push(fruta); // push adiciona um novo elemento no final da lista
+    }
+    
+}
+
+console.log(`A segunda fruta inserida na lista é: ${frutas[1]}`);
+
+
