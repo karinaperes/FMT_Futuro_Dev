4c1489aca081d9dbf1b93e44c9aa018e4b274b4e
Karina Peres <134704973+karinaperes@users.noreply.github.com>
2024-02-24 11:00:04 -0300

    Update numeros.js

--- Modulo_1/exercicios_sem2/numeros.js
+++ Modulo_1/exercicios_sem2/numeros.js
@@ -1,30 +1,33 @@
-const prompt = require("prompt-sync")();
-const numeros = [];
+const prompt = require("prompt-sync")()
+const numeros = []
 
-for (let n = 0; n < 5; n++ ) {
-    let numero = prompt(`Digite o ${n+1}º número: `);
-    if (numero === ""){
+for (let n = 0; n < 5; n++) {
+    let numero = prompt(`Digite o ${n + 1}º número: `);
+    if (numero === "") {
         console.log("Digite um número válido!");
         n--;
     } else {
         numeros.push(numero);
-        console.log(`Número: ${numero}`);
-    }    
+    }
 }
 
-console.log(`A lista de números é ${numeros}`);
+for (n = 0; n < 5; n++){    
+    console.log(`O ${n+1}º número é: ${numeros[n]}`)
+}
+
+console.log(`A lista de números é ${numeros}`)
 
 // Soma numeros do array
-let soma = numeros.reduce((total, num) => total + parseFloat(num), 0); // parseFloat para converter em número, se não os números da lista serão concatenados
+const soma = numeros.reduce((total, num) => total + parseInt(num), 0) // parseInt para converter em número, se não os números da lista serão concatenados
 
 
-console.log(`A soma dos números da lista é: ${soma}`);
+console.log("A soma dos números da lista é: " + soma)
 
 
 // Números em ordem crescente
-const numerosOrdenados = numeros.sort((a, b) => a - b); // Se o resultado de a - b for negativo, a deve vir antes de b na ordenação (ou seja, a é menor que b)
+const numerosOrdenados = numeros.sort((a, b) => a - b) // Se o resultado de a - b for negativo, a deve vir antes de b na ordenação (ou seja, a é menor que b)
 
-console.log('Números ordenados em ordem crescente: ' + numerosOrdenados);
+console.log('Números ordenados em ordem crescente: ' + numerosOrdenados)
 
 
 // Números pares
@@ -40,6 +43,3 @@ console.log("Os números pares da lista números são: " + numPares);
 const quadrados = numeros.map(n => n ** 2);
 
 console.log("O quadrado de cada número da lista números é: " + quadrados)
-
-
-
