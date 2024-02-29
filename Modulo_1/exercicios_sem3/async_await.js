9a614c6c09b84f604fffd6694987ccf4c638327d
Karina Peres <karinaperesg@gmail.com>
2024-02-29 19:16:48 -0300

    Inclusão exercício async_await

---
+++ Modulo_1/exercicios_sem3/async_await.js
@@ -0,0 +1,33 @@
+function getUserInfo() {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            const userInfo = {
+                id: 1,
+                nome: "Karina Peres",
+                idade: 44,
+                email: "email@email.com",
+            };
+            resolve(userInfo)
+            reject("Algo de errado aconteceu")
+            }, 2000)            
+    })
+}
+
+async function displayUserInfo() {
+
+    console.log("As informações do usuário são: ")
+
+    try {
+        const userInfo = await getUserInfo()
+
+    console.log(`ID do Usuário: ${userInfo.id}`)
+    console.log(`Nome: ${userInfo.nome}`)
+    console.log(`Idade: ${userInfo.idade}`)
+    console.log(`Email: ${userInfo.email}`) 
+
+    } catch (erro) {
+        console.log(erro)
+    }      
+}
+
+displayUserInfo()
